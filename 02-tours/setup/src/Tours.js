ae721484ae228cfc91a890633d5d53b8cb95ee47
codingwizard88 <mahmoud@mightydigits.com>
2021-09-12 14:15:21 +0300

    testing commits switching

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours}) => {
+  return <section>
+    <div className="title">
+      <h2>our tours</h2>
+      <div className='underline'></div>
+    </div>
+    <div>
+      {tours.map((tour) => {
+        {/* use ...tour to pass all of the tour parameters into the tour component */}
+        return <Tour key={tour.id} {...tour} ></Tour>
+      }
+      )}
+    </div>
+  </section>
 };
 
 export default Tours;
